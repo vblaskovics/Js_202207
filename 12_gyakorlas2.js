abff45bc6c5c3b7e01c3c70c6746ecd59e2fc337
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-07-19 11:49:53 +0200

    Add feladat

--- 12_gyakorlas2.js
+++ 12_gyakorlas2.js
@@ -3,4 +3,8 @@
 // 1. feladat
 // Írj egy függvényt, ami elosztja a bemeneti paramétert 12-vel, és visszaadja az eredményt!
 
-// 2- feladat
+// 2. feladat
+// Írj egy függvényt, ami ellenőrzi, hogy a bemeneti paramétere nagyobb-e, mint 0,
+// ha igen, akkor kiírja, hogy 'Nagyobb, mint 0' ha nem, akkor azt írja ki, hogy 
+// nem nagyobb, mint 0
+

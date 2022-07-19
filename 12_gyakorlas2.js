599365760941097482c9c1e5feaad9b311cf0cce
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-07-19 11:55:49 +0200

    Add feladatok

--- 12_gyakorlas2.js
+++ 12_gyakorlas2.js
@@ -8,3 +8,20 @@
 // ha igen, akkor kiírja, hogy 'Nagyobb, mint 0' ha nem, akkor azt írja ki, hogy 
 // nem nagyobb, mint 0
 
+// 3. feladat
+// Írj egy függvényt, ami bemeneti paraméternek két számot vár.
+// Ha a számok összege kisebb, mint 10, akkor adja vissza a számok szorzatát,
+// egyébként pedig adja vissza a számok különbségét.
+
+// 4. feladat
+// Írj egy függvényt, ami bemenetként 4 paramétert vár.
+// Írd ki minden bemeneti paraméternek a típusát,
+// majd térj vissza a bemeneti paraméterek összegének 100-szorosával.
+
+// 5. feladat
+// Írj egy függvényt, ami bementként két számot vár.
+// Ha az első szám nagyobb, mint a második, akkor az első szám értékét növeld eggyel,
+// a második szám értékét pedig csökkentsd eggyel, majd írasd ki mindkét számot!
+// Ha az első szám kisebb, mint a második, akkor mindkét számot emeld négyzstre 
+// (szorozd be saját magával), majd írasd ki mindkét számot!
+// Ha a két szám egyenlő, akkor ne csinálj semmit és ne is írj ki semmit!

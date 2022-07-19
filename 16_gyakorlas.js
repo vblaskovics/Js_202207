7845a00921c3a3d9d8f3aec87d1fb37c2a56ae77
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-07-19 16:12:52 +0200

    16-os gyakorlo feladat

---
+++ 16_gyakorlas.js
@@ -0,0 +1,21 @@
+// 1. feladat
+// Írj egy függvényt, ami bemenetként egy stringet kap, és kiírja, ha az üres.
+// urese("") -> üres
+
+// 2. feladat
+// Írj egy függvényt, ami két string-et vár, és az elsőben megkeresi a másodikat.
+// Ha nem található benne, akkor írja ki azt, hogy nem találjható.
+// Ha megtalálja, akkor térjen vissza a második pozíciójával!
+// keres("a b c", "b") -> 2 
+// keres("a b c", "d") -> Kiírja, hogy 'Nem található'
+
+// 3. feladat
+// Írj egy függvényt, ami egy magyar mobil számból kiszedi a hívószámot!
+// hivoszam('+36307503040') -> 30
+
+// 4. feladat
+// Írj egy függvényt, ami megnézi, hogy a bemeneti szövegben van-e @karakter, 
+// és ha van, akkor csupa nagybetűvel kiírja, egyébként csupa kisbetűvel írja ki!
+// kukacolo('asd@gmail.hu') -> ASD@GMAIL.HU
+// kukacolo('AsztaL') -> asztal
+

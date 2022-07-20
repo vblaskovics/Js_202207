a0fc86f126b069dbcf7814ccc1ba3194b29198c1
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-07-20 11:36:24 +0200

    Add 19 gyak

---
+++ 19_gyakorlas.js
@@ -0,0 +1,24 @@
+// 1. Feladat
+// Hozz létre egy 5 elemű tömböt (tetszőleges adattal)!
+
+// 2. Feladat
+// Adj hozzá 3 új elemet.
+
+// 3. Feladat
+// Módosítsd a 2-dik és a 4-dik elemét.
+
+// 4. Feladat
+// Adj hozzá 1 elemet a push metódussal!
+// Majd ezt távolítsd is el a pop metódussal!
+
+// 5. feladat
+// Töröld ki a 2-4-dik található elemeket!
+
+// 6. Feladat
+// Írasd ki a tömb hosszát!
+
+// 7. Feladat
+// Fűzz hozzá egy másik 2 elemű tömböt!
+
+// 8. Feladat
+// Töröld ki az első elemet (ne maradjon üres hely)!

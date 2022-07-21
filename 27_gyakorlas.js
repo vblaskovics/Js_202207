124c07a50eaf26a29665d144602382c5290b16ad
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-07-21 15:28:01 +0200

    27

--- 27_gyakorlas.js
+++ 27_gyakorlas.js
@@ -2,4 +2,11 @@
 // Hozz létre egy tömböt, amiben 10-esével vannak a számok 0-tól 1000-ig
 // [0, 10, 20, 30 ....]
 
+// 2. feladat
+// Írj függvényt, ami bemenetnek egy tömböt vár, és ellenőrzi, hogy a tömb
+// összes eleme szám-e! true-val térjen vissza, ha igen, false-al egyébként.
+// Pl.:
+// mindegyikSzam([2,3,4]) => true
+// mindegyikSzam([2,"alma",4]) => false
 
+// 3. feladat
